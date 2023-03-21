d7daf6ac86297d4deb3c8a21b44df63122da2673
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2023-03-21 20:16:52 +0700

    Configuration Create Asset

--- lib/config/app_constant.dart
+++ lib/config/app_constant.dart
@@ -3,4 +3,6 @@ class AppConstant {
 
   /// bareURL = 'http://192.168.127.178/course_api_asset_management'
   static const baseURL = 'http://192.168.127.178/course_api_asset_management';
+  static const baseURL2 = 'http://192.168.0.110/course_api_asset_management';
+
 }
--- lib/pages/asset/create_asset_page.dart
+++ lib/pages/asset/create_asset_page.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:d_info/d_info.dart';
 import 'package:d_input/d_input.dart';
 import 'package:d_method/d_method.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+import '../../config/app_constant.dart';
 
 class CreateAssetPage extends StatefulWidget {
   const CreateAssetPage({super.key});
@@ -38,6 +43,53 @@ class _CreateAssetPageState extends State<CreateAssetPage> {
       setState(() {});
     }
     DMethod.printBasic('imageName: $imageName');
+  }
+
+  save() async {
+    bool isValidInput = formKey.currentState!.validate();
+    // If not valid, will stop / return
+    if (!isValidInput) return;
+
+    // If valid, Go on
+
+    // If not have image, stop / return
+    if (imageByte == null) {
+      DInfo.toastError("Image Don't empty");
+      return;
+    }
+
+    // If have image, Go on
+    Uri url = Uri.parse('${AppConstant.baseURL2}/assets/create.php');
+
+    try {
+      // Method POST
+      final response = await http.post(url, body: {
+        'name': edtName.text,
+        'type': type,
+        'image': imageName,
+        'base64code': base64Encode(imageByte!),
+      });
+      DMethod.printBasic('Execute request to API');
+      DMethod.printResponse(response);
+
+      debugPrint('Response body before decoding and casting to map: ');
+      debugPrint(response.body.toString());
+
+      Map respondBody = json.decode(response.body);
+
+      debugPrint('responseBody is: ');
+      debugPrint(respondBody.toString());
+
+      bool success = respondBody['success'] ?? false;
+      if (success) {
+        DInfo.toastSuccess('Success Create New Asset');
+        Navigator.pop(context);
+      } else {
+        DInfo.toastError('Failed Create New Asset');
+      }
+    } catch (e) {
+      DMethod.printTitle('Catch', e.toString());
+    }
   }
 
   @override
@@ -117,15 +169,15 @@ class _CreateAssetPageState extends State<CreateAssetPage> {
                     onPressed: () {
                       pickImage(ImageSource.camera);
                     },
-                    icon: Icon(Icons.camera_alt),
-                    label: Text('Camera'),
+                    icon: const Icon(Icons.camera_alt),
+                    label: const Text('Camera'),
                   ),
                   OutlinedButton.icon(
                     onPressed: () {
                       pickImage(ImageSource.gallery);
                     },
-                    icon: Icon(Icons.image),
-                    label: Text('Gallery'),
+                    icon: const Icon(Icons.image),
+                    label: const Text('Gallery'),
                   ),
                 ],
               ),
@@ -134,7 +186,8 @@ class _CreateAssetPageState extends State<CreateAssetPage> {
               ),
               ElevatedButton(
                   onPressed: () {
-                    print(type);
+                    save();
+                    debugPrint(type);
                   },
                   child: const Text('Save'))
             ],
